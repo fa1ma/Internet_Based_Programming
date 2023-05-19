1f63e9939718b350b56504c75112a917913d467a
Fatma Kısa <100237151+fa1ma@users.noreply.github.com>
2023-05-19 19:02:41 +0300

    Update process.php

--- Lab_Application/process.php
+++ Lab_Application/process.php
@@ -5,17 +5,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $gender = $_POST['gender'];
 
-    $servername = "127.0.0.1"; // Replace with your MySQL server name
-    $username = "root"; // Replace with your MySQL username
-    $password = ""; // Replace with your MySQL password
-    $dbname = "lab_application"; // Replace with your MySQL database name
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $dbname = "lab_application";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-
     $sql = "INSERT INTO student (id, full_name, email, gender) VALUES ('$id','$full_name', '$email', '$gender')";
 
     if ($conn->query($sql) === true) {
@@ -23,9 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $response = "Error: " . $sql . "<br>" . $conn->error;
     }
-
     $conn->close();
-
     header("Location: index.html?response=" . urlencode($response));
     exit();
 }
